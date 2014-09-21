e5013c0f458e79d56944c7b479f4156a8fa9256e
Iaroslav Sergieiev <noomorph@gmail.com>
2014-09-21 21:05:07 +0300

    travis CI fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -333,6 +333,13 @@ module.exports = function (grunt) {
             ]
         },
 
+        karma: {
+            unit: {
+                configFile: 'test/karma.conf.js',
+                singleRun: true
+            }
+        },
+
         yaml: {
             indexer: {
                 options: {
